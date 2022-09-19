fe00adf2e4696c43aa1b677ae4620a7157d56f6a
OddbitCoder <oddbitcoder@gmail.com>
2022-09-19 14:07:10 +0200

    Updates glorkz preprocessor

--- src/adv0/tools/GlorkzProcessor.cs
+++ src/adv0/tools/GlorkzProcessor.cs
@@ -1,14 +1,22 @@
-using System.Web;
+using System.Collections;
 
 var lines = File.ReadAllLines(@"C:\Work\idp-games\src\adv0\glorkz");
 
-var crlf = "\r\n";
+var crlf = "\n";
 
-var msg_1 = "";
-var len_1 = 0;
-int id_1 = 1;
+var msg = "";
+var msg_len = 0;
+int msg_id = 1;
+int offset = 0;
 
 int sect = 1;
+
+var travWriter = new BinaryWriter(File.Open("C:\\Work\\idp-games\\src\\adv0\\trav.bin", FileMode.Create));
+var vocWriter = new BinaryWriter(File.Open("C:\\Work\\idp-games\\src\\adv0\\voc.bin", FileMode.Create));
+var txtWriter = new BinaryWriter(File.Open("C:\\Work\\idp-games\\src\\adv0\\text.bin", FileMode.Create));
+
+int[] cond = new int[141];
+var fixd = new List<int>();
 
 foreach (var line in lines.Select(l => l.Trim())) 
 {
@@ -28,28 +36,102 @@ foreach (var line in lines.Select(l => l.Trim()))
     if (sect == 1 || sect == 2 || sect == 5 || sect == 6 || sect == 10 || sect == 12)
     {
         int msgId = Convert.ToInt32(split[0]);
-        if (id_1 != msgId)
+        if (msg_id != msgId && msg_len > 0)
         {
-            Console.WriteLine($"\t{{ \"{HttpUtility.JavaScriptStringEncode(msg_1)}\", {len_1} }},");
-            //Console.WriteLine();
-            msg_1 = "";
-            id_1 = msgId;
-            len_1 = 0;
+            Console.WriteLine($"{{ {offset}, {msg_len} }}, /* {msg} */");
+            txtWriter.Write(msg.Select(ch => (byte)ch).ToArray());
+            offset += msg_len;
+            msg = "";
+            msg_id = msgId;
+            msg_len = 0;
         }
         if (msgId != -1)
         {
             var msgPart = split[1].Trim();
-            len_1 += (msg_1 == "" ? 0 : 2) + msgPart.Length;
-            msg_1 += (msg_1 == "" ? "" : crlf) + msgPart;
+            msg_len += (msg == "" ? 0 : crlf.Length) + msgPart.Length;
+            msg += (msg == "" ? "" : crlf) + msgPart;
+        }
+        else
+        {
+            msg_id = 1;
         }
     }
     else if (sect == 4)
     {
-        var wordId = Convert.ToInt32(split[0]);
+        int wordId = Convert.ToInt32(split[0]);
         if (wordId != -1)
         {
             var word = split[1].Trim();
-            Console.WriteLine($"vocab(\"{HttpUtility.JavaScriptStringEncode(word)}\",-2,{wordId});");
+            //Console.WriteLine($"vocab(\"{HttpUtility.JavaScriptStringEncode(word)}\",-2,{wordId});");
+            Console.WriteLine($"Writing entry for '{word}'...");
+            vocWriter.Write((byte)word.Length);
+            vocWriter.Write((ushort)wordId);
+            vocWriter.Write(word.Select(ch => (byte)ch).ToArray());
         }
     }
+    else if (sect == 3)
+    {
+        int id = Convert.ToInt32(split[0]);
+        if (id != -1)
+        {
+            var parts = split[1].Split('\t');
+            int n1 = Convert.ToInt32(parts[0]);
+            int n = n1 % 1000; // "tloc"
+            int m = n1 / 1000; // "conditions"
+            foreach (var part in parts.Skip(1).Select(x => Convert.ToInt32(x)))
+            {
+                if (part > 255 || part == 0) { Console.WriteLine(part); }
+            }
+            int tverb_count = parts.Skip(1).Count();
+            Console.WriteLine($"Writing trav {id}; tverb list size = {tverb_count}");
+            travWriter.Write((byte)tverb_count);
+            travWriter.Write((byte)id);
+            travWriter.Write((ushort)n);
+            travWriter.Write((ushort)m);
+            foreach (int tverb in parts.Skip(1).Select(x => Convert.ToInt32(x)))
+            {
+                travWriter.Write((byte)tverb);
+            }
+        }
+    }
+    else if (sect == 9)
+    {
+        int bit = Convert.ToInt32(split[0]);
+        if (bit != -1)
+        {
+            var parts = split[1].Split('\t');
+            foreach (var idx in parts.Select(x => Convert.ToInt32(x)))
+            {
+                cond[idx] |= 2 ^ bit;
+            }
+        }
+    }
+    else if (sect == 7)
+    {
+        int idx = Convert.ToInt32(split[0]);
+        if (idx != -1)
+        {
+            var parts = split[1].Split('\t');
+            Console.WriteLine($"{parts[0]},");
+            fixd.Add(parts.Length == 2 ? Convert.ToInt32(parts[1]) : 0);
+        }
+    }
+}
+
+travWriter.Close();
+vocWriter.Close();
+txtWriter.Close();
+
+Console.WriteLine("*** 9 ***");
+
+foreach (var item in cond) 
+{
+    Console.WriteLine($"{item},");
+}
+
+Console.WriteLine("*** 7 fixd ***");
+
+foreach (var item in fixd)
+{
+    Console.WriteLine($"{item}, ");
 }
