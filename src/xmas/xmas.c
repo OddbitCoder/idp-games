e950210880014cf56b85ad74140a85030c709f16
OddbitCoder <oddbitcoder@gmail.com>
2022-12-23 22:23:23 +0100

    SDCC at 137 chars GCC at 115 chars

--- src/xmas/xmas.c
+++ src/xmas/xmas.c
@@ -1,8 +1,3 @@
 #include<stdio.h>
-#define X(x,y) y>4&&x>y-5&&x<23-y||y<14&&x>13-y&&x<5+y
-main(){
-	puts("#include<stdio.h>");
-	puts("#define X(x,y) y>4&&x>y-5&&x<23-y||y<14&&x>13-y&&x<5+y");
-	puts("main(){int x,y=0;for(;y++<17;puts(\"\"))for(x=0;x++<17;)putchar(X(x,y)||X(y,x)?42:32);}");
-	int x,y=0;for(;y++<17;puts(""))for(x=0;x++<17;)putchar(X(x,y)||X(y,x)?42:32);
-}
+#define X(x,y)y>4&x>y-5&x<23-y|y<14&x>13-y&x<5+y
+int x;main(){for(;x++<1440;)putchar(X(x%80,x/80)|X(x/80,x%80)?42:32);}
