17c05ee295df61c62a0d350bbe7c25fe999ff8f6
OddbitCoder <oddbitcoder@gmail.com>
2023-02-20 18:07:32 +0100

    Partner ver 117 chars, GCC terminal-independent version (96 chars), GCC terminal-dependent ver (90 chars)

--- src/xmas/gcc/xmas.c
+++ src/xmas/gcc/xmas.c
@@ -1,1 +1,3 @@
-i,x,y;main(){for(;i++<1360;){x=abs(i%80-9);y=abs(i/80-8);putchar(x<5&x>y-5|y<5&y>x-5?42:32);}}
+//i,x;main(y){for(;i++<1360;)x=abs(i%80-9),y=abs(i/80-8),putchar(x>4&y>4|abs(x-y)>4?32:42);}
+// independent of terminal size
+i,x;main(y){for(;i++<306;)x=abs(i%18-9),y=abs(i/18-8),putchar(x>8?10:x>4&y>4|abs(x-y)>4?32:42);}
--- src/xmas/xmas.c
+++ src/xmas/xmas.c
@@ -1,3 +1,2 @@
-#include<stdio.h>
-#define X(x,y)y>4&x>y-5&x<23-y|y<14&x>13-y&x<5+y
-int x;main(){for(;x++<1440;)putchar(X(x%80,x/80)|X(x/80,x%80)?42:32);}
+// 117 characters
+int putchar(int),abs(int),i,x,y;main(){for(;i++<1360;)x=abs(i%80-9),y=abs(i/80-8),putchar(x>4&y>4|abs(x-y)>4?32:42);}
